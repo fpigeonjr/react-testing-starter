4dc8b7889607e320b7be64a822247b2f2699d122
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2024-05-19 21:53:22 -0500

    test: Add edit button to UserAccount component test

--- tests/components/UserAccount.test.tsx
+++ tests/components/UserAccount.test.tsx
@@ -16,15 +16,16 @@ const userAccount: User = {
 describe('User', () => {
   it('should display the name of the person logged in', () => {
     render(<UserAccount user={adminAccount}/>)
-    const name = screen.getByText(/frank/i)
+    const name = screen.getByText(adminAccount.name)
     expect(name).toBeInTheDocument()
   })
   it('should display edit button when admin', () => {
     render(<UserAccount user={adminAccount}/>)
     const editButton = screen.getByRole('button')
     expect(editButton).toBeInTheDocument()
+    expect(editButton).toHaveTextContent(/edit/i)
   })
-  it('should not display  edit button for non-admin', () => {
+  it('should not display edit button for non-admin', () => {
     render(<UserAccount user={userAccount}/>)
     const editButton = screen.queryByRole('button')
     expect(editButton).not.toBeInTheDocument()
