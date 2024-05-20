a7df76057c431bf7392deed73ca9496957d78c10
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2024-05-19 21:44:32 -0500

    test: Add UserAccount component tests
    This commit adds tests for the UserAccount component. It verifies that the name of the logged-in user is displayed correctly and that the edit button is only shown for admin users.

---
+++ tests/components/UserAccount.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react'
+import UserAccount from '../../src/components/UserAccount'
+import { User } from '../../src/entities'
+
+const adminAccount: User = {
+  id: 1,
+  name: 'Frank',
+  isAdmin: true
+}
+const userAccount: User = {
+  id: 2,
+  name: 'Alice',
+  isAdmin: false
+}
+
+describe('User', () => {
+  it('should display the name of the person logged in', () => {
+    render(<UserAccount user={adminAccount}/>)
+    const name = screen.getByText(/frank/i)
+    expect(name).toBeInTheDocument()
+  })
+  it('should display edit button when admin', () => {
+    render(<UserAccount user={adminAccount}/>)
+    const editButton = screen.getByRole('button')
+    expect(editButton).toBeInTheDocument()
+  })
+  it('should not display  edit button for non-admin', () => {
+    render(<UserAccount user={userAccount}/>)
+    const editButton = screen.queryByRole('button')
+    expect(editButton).not.toBeInTheDocument()
+  })
+})
