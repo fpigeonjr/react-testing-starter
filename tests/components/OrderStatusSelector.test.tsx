d78f408ade3ebd705b5f281168ab94971fe5ef05
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2024-06-08 19:40:56 -0500

    chore: Update OrderStatusSelector test to include getOption function

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -16,7 +16,8 @@ describe("OrderStatusSelector", () => {
       user,
       onChange,
       button: screen.getByRole("combobox"),
-      getOptions: async () => await screen.findAllByRole("option")
+      getOptions: async () => await screen.findAllByRole("option"),
+      getOption: async (label: RegExp) => await screen.findByRole("option", { name: label })
     }
   }
   it("should render New as the default status", () => {
@@ -33,5 +34,40 @@ describe("OrderStatusSelector", () => {
     expect(options).toHaveLength(3)
     const labels = options.map((option) => option.textContent)
     expect(labels).toEqual(["New", "Processed", "Fulfilled"])
+  })
+  it("should call onChange when status is changed to new", async () => {
+    const { button, user, onChange, getOptions } = renderComponent()
+
+    await user.click(button)
+    const options = await getOptions()
+
+    await user.click(options[0])
+
+    expect(onChange).not.toHaveBeenCalled()
+  })
+  it.each([
+    { label: /processed/i, value: "processed" },
+    { label: /fulfilled/i, value: "fulfilled" }
+  ])("should call onChange when status is changed to $value", async ({ label, value }) => {
+    const { button, user, onChange, getOption } = renderComponent()
+
+    await user.click(button)
+    const option = await getOption(label)
+    await user.click(option)
+
+    expect(onChange).toHaveBeenCalledWith(value)
+  })
+  it("should call onChange when status is changes to 'new'", async () => {
+    const { button, user, onChange, getOption } = renderComponent()
+
+    await user.click(button)
+    const processedOption = await getOption(/processed/i)
+    await user.click(processedOption)
+
+    await user.click(button)
+    const newOption = await getOption(/new/i)
+    await user.click(newOption)
+
+    expect(onChange).toHaveBeenCalledWith("new")
   })
 })
