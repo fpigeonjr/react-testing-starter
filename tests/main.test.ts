bb17b3564b446e1f2af5d6e3e8612fb91a91527c
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2024-06-15 16:28:47 -0500

    chore: Add msw dependency for mocking server in tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,11 @@
-import { it, expect, describe } from 'vitest'
+import { it, expect, describe } from "vitest"
 
- describe('group', () => {
-  it('should', () => {
+describe("group", () => {
+  it("should", async () => {
+    const res = await fetch("/categories")
+    const data = await res.json()
+    console.log([data])
     expect(1).toBeTruthy()
+    expect(data).toHaveLength(3)
   })
- })
+})
