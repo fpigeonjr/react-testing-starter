6d4af9cabdbc4b2cd22702b62111cdbd954dcdfb
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2024-06-16 21:49:33 -0500

    refactor: Update mock handlers in tests to use separate data file

--- tests/components/ProductList.test.tsx
+++ tests/components/ProductList.test.tsx
@@ -2,14 +2,30 @@ import { render, screen } from "@testing-library/react"
 import { HttpResponse, http } from "msw"
 import ProductList from "../../src/components/ProductList"
 import { server } from "../mocks/server"
+import { db } from "../mocks/db"
 
 describe("ProductList", () => {
+  const productIds: number[] = []
+
+  beforeAll(() => {
+    // eslint-disable-next-line no-extra-semi
+    ;[1, 2, 3].forEach(() => {
+      const product = db.product.create()
+      productIds.push(product.id)
+    })
+  })
+
+  afterAll(() => {
+    db.product.deleteMany({ where: { id: { in: productIds } } })
+  })
+
   it("should render list of products", async () => {
     render(<ProductList />)
     const items = await screen.findAllByRole("listitem")
 
     expect(items.length).toBeGreaterThan(0)
   })
+
   it("should render no products available if no products are found", async () => {
     server.use(http.get("/products", () => HttpResponse.json([])))
     render(<ProductList />)
--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,24 +1,3 @@
-import { http, HttpResponse } from "msw"
-import { products, categories } from "./data"
+import { db } from "./db"
 
-export const handlers = [
-  http.get("/categories", () => {
-    return HttpResponse.json(categories)
-  }),
-  http.get("/products", () => {
-    return HttpResponse.json(products)
-  }),
-  http.get("/products/:id", ({ params }) => {
-    const { id } = params
-    if (!id) {
-      return new HttpResponse(null, { status: 400 })
-    }
-
-    const product = products.find((product) => product.id === Number(id))
-    if (!product) {
-      return new HttpResponse(null, { status: 404 })
-    }
-
-    return HttpResponse.json(product ? product : null)
-  })
-]
+export const handlers = [...db.product.toHandlers("rest")]
